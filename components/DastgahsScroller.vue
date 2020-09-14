8231580cfa037c61c6a784bfba25d7963bcde5a6
Saeed Heidari <saeed.heidari86@gmail.com>
2020-09-14 21:59:24 +0430

    feat: dastgah

---
+++ components/DastgahsScroller.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="horizontalScroller">
+    <h3>دستگاه / آواز ({{ dastgahs.length }})</h3>
+    <div class="scroll">
+      <ul class="dastgahs">
+        <li v-for="d in dastgahs" :key="d.title">
+          <f7-link href="/about" class="dastgah-box">
+            <div class="dastgah-box-overlay"></div>
+            <h4>{{ d.title }}</h4>
+            <div class="dastgah-info">تعداد برنامه: {{ d.count }}</div>
+          </f7-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.horizontalScroller {
+  padding: 0 10px;
+  h3 {
+    margin-bottom: 0;
+    font-family: IranSansWeb;
+    font-weight: 400;
+    font-size: 1rem ;
+  }
+  .scroll {
+    overflow-x: auto;
+  }
+  ul.dastgahs {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    li {
+      width: 320px;
+      height: 120px;
+      margin-left: 10px;
+      .dastgah-box {
+        width: 320px;
+        height: 120px;
+        background: rgba(0, 0, 0, 0.1);
+        border-radius: 7.5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        background-image: url("~assets/images/dastgah-bg.jpg");
+        background-size: cover;
+        background-position: 100%;
+        position: relative;
+        .dastgah-box-overlay {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background-color: rgba($color: #000000, $alpha: 0.6);
+          border-radius: 7.5px;
+        }
+        h4 {
+          margin: 0;
+          padding: 0;
+          font-family: IranSansWeb;
+          font-weight: lighter;
+          font-size: 1.5rem;
+          position: relative;
+          z-index: 2;
+          color: #ffffff;
+        }
+        .dastgah-info {
+          font-family: IranSansWeb;
+          font-weight: lighter;
+          font-size: 0.75rem;
+          margin-top: 15px;
+          position: relative;
+          z-index: 2;
+          color: #ffffff;
+        }
+      }
+    }
+  }
+}
+</style>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["dastgahs"]
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,24 +7,7 @@
     <div v-if="!loading">
       <ProgramsScroller :programs="programs" />
       <SingersScroller :singers="singers" />
-
-      <f7-list class="searchbar-hide-on-search">
-        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-      </f7-list>
+      <DastgahsScroller :dastgahs="dastgahs" />
     </div>
   </f7-page>
 </template>
@@ -32,16 +15,18 @@
 <script>
 export default {
   data() {
-    return { programs: [], singers: [], loading: true };
+    return { programs: [], singers: [], dastgahs: [], loading: true };
   },
   async fetch() {
     const promises = [
       this.$axios.get("https://api.radio-golha.com/api/v1/program"),
-      this.$axios.get("https://api.radio-golha.com/api/v1/person/role/singer")
+      this.$axios.get("https://api.radio-golha.com/api/v1/person/role/singer"),
+      this.$axios.get("https://api.radio-golha.com/api/v1/dastgah")
     ];
     const response = await Promise.all(promises);
     this.programs = response[0].data.payload.programs;
     this.singers = response[1].data.payload.persons;
+    this.dastgahs = response[2].data.payload.dastgah;
     this.loading = false;
   },
   mounted() {
