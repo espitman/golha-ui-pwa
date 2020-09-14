6dd1b9999c3bab5d11e19d277e231ec7e211e277
Saeed Heidari <saeed.heidari86@gmail.com>
2020-09-14 21:28:51 +0430

    feat: loader

---
+++ components/FullPageLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fullPageLoader">
+    <f7-preloader :size="62"></f7-preloader>
+    <h6>درحال دریافت اطلاعات...</h6>
+  </div>
+</template>
+<style lang="scss">
+.fullPageLoader {
+  widows: 100vw;
+  height: calc(100vh - 100px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 15px;
+  font-family: IranSansWeb;
+  font-weight: lighter;
+  font-size: 1.5rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,36 @@
     <f7-navbar>
       <Navbar />
     </f7-navbar>
-    <ProgramsScroller :programs="programs" />
-    <SingersScroller :singers="singers" />
+    <FullPageLoader v-if="loading" loading="loading" />
+    <div v-if="!loading">
+      <ProgramsScroller :programs="programs" />
+      <SingersScroller :singers="singers" />
 
-    <f7-list class="searchbar-hide-on-search">
-      <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-      <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-      <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-      <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-      <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-      <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-      <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-      <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-      <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-      <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-      <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
-      <f7-list-item title="Test 1" link="/test/"></f7-list-item>
-      <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
-      <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
-      <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
-    </f7-list>
+      <f7-list class="searchbar-hide-on-search">
+        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
+        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
+        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
+        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
+        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
+        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
+        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
+        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
+        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
+        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
+        <f7-list-item title="About Nuxt7" link="/about/"></f7-list-item>
+        <f7-list-item title="Test 1" link="/test/"></f7-list-item>
+        <f7-list-item title="Tabs" link="/tabs/"></f7-list-item>
+        <f7-list-item title="Action Sheet" link="/action-sheet/"></f7-list-item>
+        <f7-list-item title="Scroller" link="/scroller/"></f7-list-item>
+      </f7-list>
+    </div>
   </f7-page>
 </template>
 
 <script>
 export default {
   data() {
-    return { programs: [], singers: [] };
+    return { programs: [], singers: [], loading: true };
   },
   async fetch() {
     const promises = [
@@ -39,6 +42,7 @@ export default {
     const response = await Promise.all(promises);
     this.programs = response[0].data.payload.programs;
     this.singers = response[1].data.payload.persons;
+    this.loading = false;
   },
   mounted() {
     this.$store.commit("todo/add", "saeed");
