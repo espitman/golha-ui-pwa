3cf714a94bedc8d633f5df15c8894da7fa02e8ff
Saeed Heidari <saeed.heidari86@gmail.com>
2020-10-28 13:16:14 +0330

    feat: SideBar Component

---
+++ components/SideBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <f7-panel right resizable>
+    <f7-view>
+      <f7-page>
+        <f7-block>Panel content2</f7-block>
+      </f7-page>
+    </f7-view>
+  </f7-panel>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,13 @@
 <template>
   <div>
-    <f7-panel right resizable>
-      <f7-view>
-        <f7-page>
-          <f7-block>Left panel content</f7-block>
-        </f7-page>
-      </f7-view>
-    </f7-panel>
+    <SideBar />
     <nuxt />
     <Player />
     <TrackAction />
   </div>
 </template>
 
-<style lang="scss">
-html,
-body {
-  font-size: 16px;
-  overflow-x: hidden;
-}
-</style>
+
 
 <script>
 import "vue-range-slider/dist/vue-range-slider.css";
