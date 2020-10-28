43817233a5a14266b3c429b284e5852297f4a9f9
Saeed Heidari <saeed.heidari86@gmail.com>
2020-10-29 01:24:18 +0330

    feat: simple sidebar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -13,6 +13,16 @@
             </div>
           </div>
         </div>
+        <div class="bot">
+          <f7-list>
+            <f7-list-item link="#" title="ورود اعضا">
+              <f7-icon slot="media" material="home"></f7-icon>
+            </f7-list-item>
+            <f7-list-item link="#" title="درباره ما">
+              <f7-icon slot="media" material="info_circle"></f7-icon>
+            </f7-list-item>
+          </f7-list>
+        </div>
       </f7-page>
     </f7-view>
   </f7-panel>
@@ -59,6 +69,56 @@
           margin-top: 8px;
           font-size: 0.75em;
         }
+      }
+    }
+  }
+  .bot {
+    .item-media {
+      margin-right: 10px;
+    }
+    .item-inner {
+      padding-right: 16px;
+      .item-title {
+        font-family: IranSansWeb;
+        font-weight: 300;
+        width: 100%;
+        &::after {
+          font-family: "framework7-core-icons";
+          font-weight: normal;
+          font-style: normal;
+          line-height: 1;
+          letter-spacing: normal;
+          text-transform: none;
+          white-space: nowrap;
+          word-wrap: normal;
+          direction: ltr;
+          -webkit-font-smoothing: antialiased;
+          text-rendering: optimizeLegibility;
+          -moz-osx-font-smoothing: grayscale;
+          font-feature-settings: "liga";
+          text-align: center;
+          display: block;
+          width: 100%;
+          height: 100%;
+          font-size: 20px;
+          position: absolute;
+          top: 50%;
+          width: 8px;
+          height: 14px;
+          margin-top: -7px;
+          font-size: 20px;
+          font-size: var(--f7-list-chevron-icon-font-size);
+          line-height: 14px;
+          color: rgba(0, 0, 0, 0.2);
+          color: var(--f7-list-chevron-icon-color);
+          pointer-events: none;
+          left: 0;
+          content: "chevron_left";
+          content: var(--f7-list-chevron-icon-left);
+        }
+      }
+      &::before {
+        all: unset;
       }
     }
   }
